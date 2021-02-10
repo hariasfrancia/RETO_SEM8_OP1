14747a7b459fac80dc7de290ee4650fe7798af2d
Héctor Arias Francia <hariasfrancia@gmail.com>
2021-02-10 17:59:26 -0500

    update

---
+++ js/tareas.js
@@ -0,0 +1,10 @@
+class tarea {
+    constructor(nombre, descripcion, fecha, hora){
+      this.tarea_nombre = nombre
+      this.tarea_descripcion = descripcion
+      this.tarea_fecha = fecha
+      this.tarea_hora = hora
+
+    }
+  }
+  
